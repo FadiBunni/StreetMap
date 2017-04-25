99d6591129679c25ea755bdd922c067c6f01cd0e
FadiBunni <fadibunni_guitar@hotmail.com>
2017-04-25 13:33:33 +0200

    Nyeste!

--- client/js/bundle/sketch.js
+++ client/js/bundle/sketch.js
@@ -64,6 +64,9 @@ Roads[Object.keys(Roads).length] = new Road("50 90", "LarslejStraede", "35 120" 
 Roads[Object.keys(Roads).length] = new Road("10 70", "Noerrevoldgade", "25 100" , false);
 Roads[Object.keys(Roads).length] = new Road("25 100", "Noerrevoldgade", "35 120" , false);
 Roads[Object.keys(Roads).length] = new Road("35 120", "Noerrevoldgade", "60 150" , false);
+Roads[Object.keys(Roads).length] = new Road("80 70", "Vestergade", "55 55" , false);
+Roads[Object.keys(Roads).length] = new Road("55 55", "Vestergade", "35 35" , false);
+
 
 for(var intersect in Intersections){
 	var inter = Intersections[intersect];
@@ -72,23 +75,95 @@ for(var intersect in Intersections){
 
 for(var road in Roads){
 	var r = Roads[road];
-	//console.log(r.length);
+	//console.log("Road name: " + r.name + " First coordinate: "+ r.firstCoord + " Second coordinate: " + r.secondCoord + " Length: " + r.length);
+}
+
+
+//A* algorithm
+  var openSet = [];
+  var closedSet = [];
+  var start;
+  var end;
+  var w,h;
+
+function Spot(p,i,j){
+  this.x = i;
+  this.y = j;
+  this.f = 0;
+  this.g = 0;
+  this.h = 0;
+
+  this.show = function(color){
+    p.fill(color);
+    p.noStroke();
+    p.rect(this.x*w,this.y*h,w-1,h-1);
+  }
 }
 
 var s = function(p) {
 
+  var cols = 5;
+  var rows = 5;
+  var grid = new Array(cols);
+
   p.setup = function() {
-    var canvas = p.createCanvas(700, 410);
+    //create canvas and center it
+    var canvas = p.createCanvas(400, 400);
     centerCanvas(canvas);
+
+    w = p.width / cols;
+    h = p.height / rows;
+
+    //Making a 2d array;
+    for(var i = 0; i < cols; i++){
+      grid[i] = new Array(rows);
+    }
+
+    for(var i = 0; i < cols; i++){
+      for(var j = 0; j< rows; j++){
+        grid[i][j] = new Spot(p,i,j);
+      }
+    }
+
+    //The ending and starting node
+    start = grid[0][0];
+    end = grid[cols - 1][rows - 1]
+
+    openSet.push(start);
+
+    //console.log(grid);
   };
 
   p.draw = function() {
+    if(openSet.length > 0){
+      //we can keep searching
+    } else {
+      // no solution
+    }
+
     p.background(0);
+
+    for(var i = 0; i < cols; i++){
+      for(var j = 0; j < rows; j++){
+        grid[i][j].show(p.color(255));
+      }
+    }
+
+    //print out open and closed list:
+    for(var i = 0; i < openSet.length; i++){
+      openSet[i].show(p.color(0,255,0));
+    }
+
+    for(var i = 0; i < closedSet.length; i++){
+      closedSet[i].show(p.color(255,0,0));
+    }
   };
 };
 
 var myp5 = new p5(s);
 
+
+//Ui function
 function centerCanvas(canvas) {
   var x = (myp5.windowWidth - myp5.width) / 2;
   var y = (myp5.windowHeight - myp5.height) / 2;
